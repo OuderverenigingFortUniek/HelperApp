d383ab1dcc05018b27bde83a9e4a345ab526a1fe
OuderverenigingFortUniek <153747807+OuderverenigingFortUniek@users.noreply.github.com>
2023-12-13 16:28:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,27 @@
 import streamlit as st
 import pandas as pd
- 
+
+def get_data():
+ df = pd.DataFrame({
+    'taken': ["ik bak pannenkoeken", "ik kom 's namiddags opstellen", "ik help bij verdelen na schooltijd"]    
+    })
+ return df
+
+if 'df' not in st.session_state:
+    st.session_state.df = get_data()
+df = st.session_state.df
+
 st.write("""
 # Helper App
 Hallo, Wintersfeer 22/12 komt eraan!
 """)
 
-df = pd.DataFrame({
-    'taken': ["ik bak pannenkoeken", "ik kom 's namiddags opstellen", "ik help bij verdelen na schooltijd"]    
-    })
-
 with st.form("my_form"):
 
-  option = st.selectbox(
-    'Hoe wil je helpen?',
-     df['taken'])
-
+  option = st.selectbox('Hoe wil u helpen?',df['taken'], index=None, placeholder="Maak uw keuze")
   name = st.text_input('Uw naam', placeholder= 'Bob')
-  st.form_submit_button('Klaar!')
 
-'U koos: ', option
+  if st.form_submit_button('Klaar!'):
+    st.session_state.df = get_data()
+   
+name , ' koos: ', option
